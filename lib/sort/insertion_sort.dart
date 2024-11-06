5109bc5ce578b21c084e5a09bce78f746b666e81
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-11-06 13:14:56 +0530

    insertion sort

--- lib/sort/insertion_sort.dart
+++ lib/sort/insertion_sort.dart
@@ -1,17 +1,30 @@
 import '../types.dart';
 
+/// Builds up the sort by gradually creating a larger left half which is always sorted.
+/// You pick a number and insert it to its correct position.
+///
+/// Time Complexity
+/// Worst Case: O(n^2)
+///
+/// Insertion sorts are useful when data is constantly coming in real-time one-by-one as we can insert the new element to its correct position rather than sorting the entire array. For example, real-time transaction processing where new orders are constantly coming in.
 Numbers insertionSort(Numbers array) {
-  for (var i = 0; i < array.length; i++) {
+  for (var i = 1; i < array.length; i++) {
     final currentValue = array[i];
     var j = i - 1;
 
-    while (j >= 0 && array[j] > currentValue) {
+    while (j >= 0 && currentValue < array[j]) {
       array[j + 1] = array[j];
       j--;
     }
-
     array[j + 1] = currentValue;
   }
 
   return array;
+}
+
+void main() {
+  final array = <int>[4, 9, 2, 40, 1];
+  final result = insertionSort(array);
+
+  print(result); // [1, 2, 4, 9, 40];
 }
