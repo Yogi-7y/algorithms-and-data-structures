eb6cf298aa697aa80956bec424da9ba28efe0767
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-01-26 14:46:01 +0530

    chore: remove leading _

--- test/sort/selection_sort_test.dart
+++ test/sort/selection_sort_test.dart
@@ -5,8 +5,8 @@ final _input = [10, 4, 2, 3];
 
 void main() {
   test('sort array using selection sort', () {
-    final _result = selectionSort(_input);
-    final _expected = [2, 3, 4, 10];
-    expect(_result, _expected);
+    final result = selectionSort(_input);
+    final expected = [2, 3, 4, 10];
+    expect(result, expected);
   });
 }
