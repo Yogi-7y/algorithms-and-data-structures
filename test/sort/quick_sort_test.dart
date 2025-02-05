83603894762b85ccb4e03966c558d413b67ff3d4
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-02-05 20:46:06 +0530

    chore: refactor code

--- test/sort/quick_sort_test.dart
+++ test/sort/quick_sort_test.dart
@@ -3,11 +3,11 @@ import 'package:test/test.dart';
 
 void main() {
   test('quick sort', () {
-    final _input = [3, 6, 2, 1, 5, 4, -1, 0, 10, 8, 7, 9, 9, -4, -8, 99];
-    final _expected = [-8, -4, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 99];
+    final input = [3, 6, 2, 1, 5, 4, -1, 0, 10, 8, 7, 9, 9, -4, -8, 99];
+    final expected = [-8, -4, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 99];
 
-    final result = quickSort(array: _input, left: 0, right: _input.length - 1);
+    final result = quickSort(array: input, left: 0, right: input.length - 1);
 
-    expect(result, _expected);
+    expect(result, expected);
   });
 }
--- test/sort/radix_sort_test.dart
+++ test/sort/radix_sort_test.dart
@@ -2,28 +2,28 @@ import 'package:algorithms_and_data_structures/sort/radix_sort.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const _number = 12345;
+  const number = 12345;
   test('should return 5, if position is 0', () {
-    expect(getDigitAt(_number, 0), 5);
+    expect(getDigitAt(number, 0), 5);
   });
 
   test('should return 4, if position is 1', () {
-    expect(getDigitAt(_number, 1), 4);
+    expect(getDigitAt(number, 1), 4);
   });
 
   test('should return 3, if position is 2', () {
-    expect(getDigitAt(_number, 2), 3);
+    expect(getDigitAt(number, 2), 3);
   });
 
   test('should return 2, if position is 3', () {
-    expect(getDigitAt(_number, 3), 2);
+    expect(getDigitAt(number, 3), 2);
   });
 
   test('should return 1, if position is 4', () {
-    expect(getDigitAt(_number, 4), 1);
+    expect(getDigitAt(number, 4), 1);
   });
 
   test('should return 0, if position is 5', () {
-    expect(getDigitAt(_number, 5), 0);
+    expect(getDigitAt(number, 5), 0);
   });
 }
